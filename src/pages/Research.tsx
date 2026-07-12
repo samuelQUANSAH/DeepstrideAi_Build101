85eb617968b72ae6665ccfc4345997dbb642caa9
Samuel Quansah <Samuelquansah@hotmail.com>
2026-07-12 15:53:29 -0700

    feat: embed StitchSandbox inside Research page view

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { QuantumHorizon } from '../components/sections/QuantumHorizon';
+import { StitchSandbox } from '../components/sections/StitchSandbox';
 
 export const Research: React.FC = () => {
   return (
     <div className="pt-24 min-h-screen">
       <QuantumHorizon />
+      <StitchSandbox />
     </div>
   );
 };
+
 export default Research;
